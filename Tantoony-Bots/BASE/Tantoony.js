686dcaec6391fefbf359d3d291e0b477a64726fe
Tantoony <tantoony.id@gmail.com>
2021-03-13 17:58:50 +0300

    Update Tantoony.js

--- Tantoony-Bots/BASE/Tantoony.js
+++ Tantoony-Bots/BASE/Tantoony.js
@@ -23,10 +23,6 @@ class Tantoony extends Client {
 
         this.invites = new Object();
 
-        this.databaseCache = {};
-        this.databaseCache.users = new Collection();
-        this.databaseCache.guilds = new Collection();
-        this.databaseCache.members = new Collection();
     }
 
     loadCommand(commandPath, commandName) {
@@ -64,112 +60,6 @@ class Tantoony extends Client {
         return false;
     }
 
-    async findOrCreateUser({ id: userID }, isLean) {
-        return new Promise(async (resolve) => {
-            if (this.databaseCache.users.get(userID)) {
-                resolve(isLean ? this.databaseCache.users.get(userID).toJSON() : this.databaseCache.users.get(userID));
-            } else {
-                let userData = (isLean ? await this.usersData.findOne({ id: userID }).lean() : await this.usersData.findOne({ id: userID }));
-                if (userData) {
-                    resolve(userData);
-                } else {
-                    userData = new this.usersData({ id: userID });
-                    await userData.save();
-                    resolve((isLean ? userData.toJSON() : userData));
-                }
-                this.databaseCache.users.set(userID, userData);
-            }
-        });
-    }
-
-    async findOrCreateGuild({ id: guildID }, isLean) {
-        return new Promise(async (resolve) => {
-            if (this.databaseCache.guilds.get(guildID)) {
-                resolve(isLean ? this.databaseCache.guilds.get(guildID).toJSON() : this.databaseCache.guilds.get(guildID));
-            } else {
-                let guildData = (isLean ? await this.guildsData.findOne({ id: guildID }).populate("members").lean() : await this.guildsData.findOne({ id: guildID }).populate("members"));
-                if (guildData) {
-                    resolve(guildData);
-                } else {
-                    guildData = new this.guildsData({ id: guildID });
-                    await guildData.save();
-                    resolve(isLean ? guildData.toJSON() : guildData);
-                }
-                this.databaseCache.guilds.set(guildID, guildData);
-            }
-        });
-    }
-
-    async resolveUser(search) {
-        let user = null;
-        if (!search || typeof search !== "string") return;
-        if (search.match(/^<@!?(\d+)>$/)) {
-            let id = search.match(/^<@!?(\d+)>$/)[1];
-            user = this.users.fetch(id).catch((err) => { });
-            if (user) return user;
-        }
-        if (search.match(/^!?(\w+)#(\d+)$/)) {
-            let username = search.match(/^!?(\w+)#(\d+)$/)[0];
-            let discriminator = search.match(/^!?(\w+)#(\d+)$/)[1];
-            user = this.users.find((u) => u.username === username && u.discriminator === discriminator);
-            if (user) return user;
-        }
-        user = await this.users.fetch(search).catch(() => { });
-        return user;
-    }
-
-    async resolveMember(search, guild) {
-        let member = null;
-        if (!search || typeof search !== "string") return;
-        if (search.match(/^<@!?(\d+)>$/)) {
-            let id = search.match(/^<@!?(\d+)>$/)[1];
-            member = await guild.members.fetch(id).catch(() => { });
-            if (member) return member;
-        }
-        if (search.match(/^!?(\w+)#(\d+)$/)) {
-            guild = await guild.fetch();
-            member = guild.members.find((m) => m.user.tag === search);
-            if (member) return member;
-        }
-        member = await guild.members.fetch(search).catch(() => { });
-        return member;
-    }
-
-    async resolveRole(search, guild) {
-        let role = null;
-        if (!search || typeof search !== "string") return;
-        if (search.match(/^<@&!?(\d+)>$/)) {
-            let id = search.match(/^<@&!?(\d+)>$/)[1];
-            role = guild.roles.get(id);
-            if (role) return role;
-        }
-        role = guild.roles.find((r) => search === r.name);
-        if (role) return role;
-        role = guild.roles.get(search);
-        return role;
-    }
-    
-    async kanalbul(kanal) {
-        const adapterchannels = this.adapterchannels;
-        const kanallar = low(adapterchannels);
-
-        var sonuc = kanallar.get(kanal).value();
-        console.log(sonuc);
-
-        return sonuc;
-
-    }
-
-    async rolbul(rol) {
-        const adapterroles = this.adapterroles;
-        const roller = low(adapterroles);
-
-        var sonuc = roller.get(rol).value();
-        console.log(sonuc);
-
-        return sonuc;
-
-    }
 
 }
 
